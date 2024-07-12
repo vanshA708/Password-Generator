75463f2fee9aed3f29201c32b7e988846cade01c
Vansh Anand <vanshanand708@gmail.com>
2024-07-12 21:10:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    document.getElementById("password-display").innerText = password;
+}
+
